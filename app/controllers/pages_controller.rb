f40d0c25884691c908c1f5eb28853bfe17e6485e
Nick Wales <nwales@homeaway.com>
2012-01-29 21:10:39 +0000

    Code cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -49,12 +49,10 @@ class PagesController < ApplicationController
     @league_players = Playerdiv.joins(:division).where(:divisions => {:season_id => @season})
     
     @players = Result.joins(:match).where(:matches => {:playerdiv_id => 1}).where(:results => {:user_id => (params[:article])})
-    
-    
 
   end
   
-  
+
   
 
 end
